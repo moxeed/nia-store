4ab07434078e2d9afc0ec0cc54279acee5af55be
moxeed <mortezabahjat@gmail.com>
2022-08-16 20:24:25 +0430

    specifications copy paste

--- src/common/components/nia-button.tsx
+++ src/common/components/nia-button.tsx
@@ -5,7 +5,7 @@ export const NiaButton = ({
                               children,
                               className
                           }: { onClick: () => void, children: ReactNode, className?: string }) => {
-    return <div className={"p-2 bg-white rounded text-center overflow-hidden " + className} onClick={onClick}>
+    return <div className={"p-2 bg-white rounded text-center overflow-hidden hover:bg-gray-100 border-gray-400 border-2 m-1 " + className} onClick={onClick}>
         {children}
     </div>
 }
--- src/product/components/specification-editor.tsx
+++ src/product/components/specification-editor.tsx
@@ -1,13 +1,20 @@
-import {Button, FlexboxGrid, Input, Panel, Stack} from "rsuite";
+import {Button, Col, FlexboxGrid, Input, Message, Panel, Row, Stack, useToaster} from "rsuite";
 import {LabelPicker} from "./label-picker";
 import {Label} from "../entities/label";
 import {Specification} from "../entities/product";
+import {NiaButton} from "../../common/components/nia-button";
+import React from "react";
+
+const message = <Message showIcon type="success">
+    با موفقیت کپی شد
+</Message>
 
 export const SpecificationEditor = (props: {
     specifications: Array<Specification>, setSpecifications: (a: Array<Specification>) => void
 }) => {
     const {setSpecifications} = props;
     const specifications = props.specifications ?? new Array<Specification>();
+    const toast = useToaster()
 
     const addLabel = (label: Label) => {
         specifications.push({
@@ -27,7 +34,29 @@ export const SpecificationEditor = (props: {
         setSpecifications(specifications);
     }
 
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(JSON.stringify(specifications.map(s => ({...s, id: undefined}))))
+            .then(() => toast.push(message))
+    }
+
+    const loadFromClipBoard = () => {
+        navigator.clipboard.readText()
+            .then(t => setSpecifications(JSON.parse(t)))
+    }
+
     return <Panel header="مشخصات" bordered className="pb-2 pt-2">
+        <Row>
+            <Col xs={12}>
+                <NiaButton onClick={copyToClipboard}>
+                    کپی کردن مشخصات
+                </NiaButton>
+            </Col>
+            <Col xs={12}>
+                <NiaButton onClick={loadFromClipBoard}>
+                    الحاق
+                </NiaButton>
+            </Col>
+        </Row>
         <LabelPicker onSelect={addLabel}/>
         {specifications?.map((spec, i) => (
             <FlexboxGrid className="pt-2 pb-2">
